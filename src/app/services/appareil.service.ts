74a3809ecd7a1622d234d7bc5ae5748d4e0656e2
Piotr KONCZYKOWSKI <piotr_konczykowski@hotmail.com>
2021-01-25 16:54:30 +0100

    all lessons

---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,108 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable()
+export class AppareilService {
+
+  appareilSubject = new Subject<any[]>();
+
+  private appareils = [
+    {
+      id: 1,
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      id: 2,
+      name: 'Télévision',
+      status: 'allumé'
+    },
+    {
+      id: 3,
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  constructor(private httpClient: HttpClient) {}
+
+  emitAppareilSubject() {
+    this.appareilSubject.next(this.appareils.slice());
+  }
+
+  getAppareilById(id: number) {
+    const appareil = this.appareils.find(
+      (appareilObject) => {
+        return appareilObject.id === id;
+      }
+    );
+    return appareil;
+  }
+
+  switchOnAll() {
+    for (let appareil of this.appareils) {
+      appareil.status = 'allumé'
+    }
+    this.emitAppareilSubject();
+  }
+
+  switchOffAll() {
+    for (let appareil of this.appareils) {
+      appareil.status = 'éteint'
+    }
+    this.emitAppareilSubject();
+  }
+
+  switchOnOne(index: number) {
+    this.appareils[index].status = 'allumé';
+    this.emitAppareilSubject();
+  }
+
+  switchOffOne(index: number) {
+    this.appareils[index].status = 'éteint';
+    this.emitAppareilSubject();
+  }
+
+  addAppareil(name: string, status: string) {
+    const appareilObject = {
+      id: 0,
+      name: '',
+      status: ''
+    };
+    appareilObject.name = name;
+    appareilObject.status = status;
+    appareilObject.id = this.appareils[(this.appareils.length - 1)].id + 1;
+
+    this.appareils.push(appareilObject);
+    this.emitAppareilSubject();
+  }
+
+  saveAppareilsToServer() {
+    this.httpClient
+      .put('https://http-client-demo-fdb25-default-rtdb.firebaseio.com/appareils.json', this.appareils)
+      .subscribe(
+        () => {
+          console.log('Enregistrement terminé !')
+        },
+        (error) => {
+          console.log('Erreur de sauvegarde ! ' + error);
+        }
+      );
+  }
+
+  getAppareilsFromServer() {
+    this.httpClient
+      .get<any[]>('https://http-client-demo-fdb25-default-rtdb.firebaseio.com/appareils.json')
+      .subscribe(
+        (response) => {
+          this.appareils = response;
+          this.emitAppareilSubject();
+        },
+        (error) => {
+          console.log('Erreur de chargement ! ' + error);
+        }
+      )
+  }
+
+}
